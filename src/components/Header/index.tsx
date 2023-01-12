aa27d48f9bbcd57ee5ada6e43a5833b05aaf5992
brandaoplaster <brandaoplaster@gmail.com>
2023-01-11 23:09:40 -0300

    feat: Adding header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Content } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
